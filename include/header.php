645da8cf15199c9d231b996b07a61af5d53535ee
Itumeleng Tsoela <thiotsoela@gmail.com>
2022-05-12 06:42:23 +0200

    formated

--- include/header.php
+++ include/header.php
@@ -24,14 +24,10 @@
         <div>
             <a href="index.php">Trade Books</a>
             <div>
-                <div>
-                    <input type="text" placeholder="Search for ISBN or book title....">
-                    <button>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-                            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-                        </svg>
-                    </button>
-                </div>
+                <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
+                    <input type="text" name="searchInput" placeholder="Search for ISBN or book title....">
+                    <input type="submit" name="searchSubmit" value="search" >
+                </form>
                 <a href="addbook.php">Sell</a>
                 <a href="trackorder.php">Track Order</a>
                 <?php 
